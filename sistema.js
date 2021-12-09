53f5ce1ef1746b0faec097f8b22f36d8cdee15f1
Julio Polistschuk <juliopolistschukw@gmail.com>
2021-12-09 19:36:08 -0300

    Feito na aula

---
+++ sistema.js
@@ -0,0 +1,38 @@
+let nomeDeAluno = ["marcel", "joslei", "creito", "jubiscreiso"]
+for(let nome of nomeDeAluno){
+    console.log(`esta pessoa se chama ${nome}`)
+}
+
+
+
+
+
+// let numeroDeAlunos = 10;
+// let contador = 0;
+
+// while (contador <= numeroDeAlunos){
+//     //console.log(contador)
+    
+//     if(contador == 0){
+//         console.log("O numero atual é zero")
+//     }else if(contador % 2 == 0){
+//         console.log("o numero " + contador + " é par")
+//     }else{
+//         console.log("O numero " + contador + " é  impar")
+//     }
+//     contador++;
+// }
+
+
+
+// for (let contador = 0; contador <= numeroDeAlunos; contador++){
+//     //console.log(contador)
+    
+//     if(contador == 0){
+//         console.log("O numero atual é zero")
+//     }else if(contador % 2 == 0){
+//         console.log("o numero " + contador + " é par")
+//     }else{
+//         console.log("O numero " + contador + " é  impar")
+//     }
+// }
